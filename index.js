046cbb937a31cc626ffa18b5987103363db37c97
Mh Mitas <mahfuzulmitas@gmail.com>
2024-05-20 13:02:53 +0600

    experiment

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jt5df8u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -55,7 +55,9 @@ async function run() {
 
         // cart collection
         app.get('/carts', async (req, res) => {
-            const result = await cartColl.find().toArray()
+            const uid = req.query?.uid
+            let query = { userId: uid }
+            const result = await cartColl.find(query).toArray()
             res.send(result)
         })
 
