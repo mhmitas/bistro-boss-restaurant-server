35655a995c461fdda94c0fec9d9ba82d929a9f25
Mh Mitas <mahfuzulmitas@gmail.com>
2024-05-24 06:05:49 +0600

    I have lost something

--- _project_docs_/module-68/steps.js
+++ _project_docs_/module-68/steps.js
@@ -4,25 +4,35 @@
 /*
 ## 68-1: Module Introduction and save user data .
 ~ When user sign up to the app save user data to the database
-
+____________________________
 ## 68-2: Google sign in and when to save user info
 ~ implement google sign in and created a different component to handle it
-
+____________________________
 ## 68-3: Save user if he does't exists in the database:
 ~ test some different methode to do it(1. making the email unique, 2 .upsert, 3. simple checking)
     - create a query using user email and find the email in the user coll. if the user already exists return with a message 
     -  
-
 ~ Woking with dashboard: 
-    - some khucra work in sidebar based on role
+    - some khucra work in sidebar based on role(created navlinks)
     
-    
-## 68-4 Load all users on the Dashboard page
+____________________________
+## 68-4 Load all users on the Dashboard page;
+~ created an api in server side. --> use useQuery() hook to get all users data in all users page.  --> Use table and display users --> implement delete user methode --> 
 
+____________________________
 ## 68-5 Display All Users And Create Make Admin API
+~ created api to update user role user to admin 
 
+____________________________
 ## 68-6 Make User Admin And Install JWT
+~ --> create a post request using axiosSecure to update user role and successfully updated it.
+=> I want to secure some api
+~ so I will use jwt() | install jwt token and require it
 
+____________________________
+## (Recap) Create a JWT token and save it on local storage
+~ create jwt token: go to jwt repo --> create a token and send it with res. --> recive this token and save it in the local storage and when log out remove this token
+~ now i will send the token with request: 
 
 
 
--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const jwt = require('jsonwebtoken')
 const app = express()
 const port = process.env.PORT || 5000
 
 // middleware
 app.use(cors())
 app.use(express.json())
+
+// my middlewares;
+const verifyToken = (req, res, next) => {
+    // console.log('from verify tokne middleware', req.headers)
+    if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'forbidden access' })
+    }
+    const token = req.headers.authorization.split(' ')[1]
+    console.log(token)
+}
 
 
 app.get('/', (req, res) => {
@@ -34,8 +45,17 @@ async function run() {
         const reviewColl = database.collection('review')
         const cartColl = database.collection('carts')
 
+        // JWT related APIs
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
+
         // Users Related APIs //
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyToken, async (req, res) => {
+            // vul val jaigai console.log korle kaj hobe na.
+            console.log(req.headers)
             const result = await userColl.find().toArray()
             res.send(result)
         })
